28c8e4974f6ce45a362fa1d34c3f3798a2ea5b03
Simran Rai <raicalvin@gmail.com>
2024-06-13 19:45:19 -0700

    initial commit with binary search

---
+++ src/BinarySearch.kt
@@ -0,0 +1,17 @@
+fun binarySearch(list: List<Int>, item: Int): Int? {
+    var lowIndex = 0
+    var highIndex = list.size - 1
+
+    while (lowIndex <= highIndex) {
+        val midIndex = (lowIndex + highIndex) / 2
+        val guess = list[midIndex]
+
+        when {
+            guess < item -> lowIndex = midIndex + 1
+            guess > item -> highIndex = midIndex - 1
+            else -> return midIndex
+        }
+    }
+
+    return null
+}
---
+++ src/Main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val position = binarySearch(listOf(1, 3, 4, 6, 7, 9, 10, 12, 14, 16), -1)
+    print(position)
+}
