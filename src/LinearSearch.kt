3b3c3d6a4747583575b4c76e4c49ecb85316a98e
Simran Rai <raicalvin@gmail.com>
2024-07-05 15:26:25 -0700

    add linear search algorithm

---
+++ src/LinearSearch.kt
@@ -0,0 +1,11 @@
+// Searches the given list for the item and returns an index of its found position or null otherwise
+fun linearSearch(list: List<Int>, item: Int): Int? {
+    var foundIndex: Int? = null
+    if (list.isEmpty()) return null
+    var i = 0
+    while (foundIndex == null && i < list.size) {
+        if (list[i] == item) foundIndex = i
+        i += 1
+    }
+    return foundIndex
+}
