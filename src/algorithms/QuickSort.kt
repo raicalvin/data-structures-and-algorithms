d2d0c3d51a70ab53d082ea0fdd8660c886d74c7c
Simran Rai <simranrai@simransacstudio.lan>
2024-08-13 18:27:35 -0700

    create the quicksort algorithm

--- src/algorithms/QuickSort.kt
+++ src/algorithms/QuickSort.kt
@@ -1,2 +1,17 @@
 package algorithms
 
+fun quickSort(list: List<Int>): List<Int> {
+    if (list.size < 2) return list
+
+    if (list.size == 2) {
+        return if (list.first() > list.last()) listOf(list.last(), list.first()) else return list
+    }
+
+    val pivot = list.first()
+    val smaller = mutableListOf<Int>()
+    val larger = mutableListOf<Int>()
+
+    list.drop(1).forEach { if (it <= pivot) smaller.add(it) else larger.add(it) }
+
+    return quickSort(smaller) + pivot + quickSort(larger)
+}
